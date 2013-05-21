bc93b7effbd4782fc33ce9221afedeedcc7626cc
Shawn Nolan <snolan760@shawnnolan.com>
2013-05-20 23:01:44 -0700

    Initial Commit
    Initial Commit

---
+++ stripVersion.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * Plugin Name: Strip Version from scripts
+ * Plugin URI: http://wellrootedmedia.com/plugins/strip-version-from-scripts/
+ * Description: This is a simple contact form with a captcha
+ * Version: 1.0
+ * Author: Well Rooted Media
+ * Author URI: http://wellrootedmedia.com
+ * License: GPL2
+ */
+add_filter( 'script_loader_src', 'remove_src_version' );
+add_filter( 'style_loader_src', 'remove_src_version' );
+
+function remove_src_version ( $src ) {
+
+    global $wp_version;
+
+    $version_str = '?ver='.$wp_version;
+    $version_str_offset = strlen( $src ) - strlen( $version_str );
+
+    if( substr( $src, $version_str_offset ) == $version_str )
+        return substr( $src, 0, $version_str_offset );
+    else
+        return $src;
+
+}
